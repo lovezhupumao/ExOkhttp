8978928b6306b9b9715e8d3bdb99b222c1036df8
ZhuPumao <20103177@cqu.edu.cn>
2016-05-21 15:57:56 +0800

    //天气jsonbean目前还没实现，只实现了号码归属地JsonBean

--- app/src/main/java/com/example/administrator/exokhttp/MainActivity.java
+++ app/src/main/java/com/example/administrator/exokhttp/MainActivity.java
@@ -5,12 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
 
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Map;
 
 import okhttp3.FormBody;
@@ -21,22 +29,32 @@ import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
     String url ="http://op.juhe.cn/onebox/weather/query";
+    private EditText edit;
+    private TextView textView;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+            edit=(EditText)findViewById(R.id.myedit);
+        textView=(TextView)findViewById(R.id.mytext);
+        btn=(Button)findViewById(R.id.btn);
 
-            new Thread(){
-                @Override
-                public void run() {
-                    try{
-
-                    new GetWeather().run();}
-                    catch (Exception e){
-                        Log.i("aaaa","Exception"+e.getCause()+e.getMessage());
-                    }
-                }
-            }.start();
+          btn.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  new Thread(){
+                      @Override
+                      public void run() {
+                          try{
+                              new GetWeather().run();}
+                          catch (Exception e){
+                              Log.i("aaaa","Exception"+e.getCause()+e.getMessage());
+                          }
+                      }
+                  }.start();
+              }
+          });
 
 
 
@@ -44,26 +62,33 @@ public class MainActivity extends AppCompatActivity {
     }
  public  class GetWeather{
      OkHttpClient client=new OkHttpClient();
-     private void run() throws  Exception{
-         RequestBody formBody = new FormBody.Builder()
-                 .add("cityname", "重庆")
-                 .add("key", "1adae4c7546c89639f2f5cc9c3a3d118")
-                 .build();
+     String string = edit.getText().toString();
+     private void run() {
          Request request = new Request.Builder()
-                 .url("http://op.juhe.cn/onebox/weather/query")
-                 .post(formBody)
+                 .url("http://apis.juhe.cn/mobile/get?phone="+string+"&key="
+                 +"2f121eb8bf260e938df638ec3cc2e5d4")
+                 .get()
                  .build();
-         Response response = client.newCall(request).execute();
-         if (!response.isSuccessful()){
-             throw new IOException("Unexpected code " + response.code());}
-         JSONObject object=JSONObject.parseObject(response.body().string());
+         try{
+             Response response = client.newCall(request).execute();
 
-         Gson gson=new Gson();
+             //System.out.println(response.body().string());
 
+             Gson gson = new Gson();
+             java.lang.reflect.Type type = new TypeToken<JsonBean>() {}.getType();
+             final JsonBean jsonBean = gson.fromJson(response.body().string(), type);
+             MainActivity.this.runOnUiThread(new Runnable() {
+                 @Override
+                 public void run() {
+                     textView.setText("归属地：" + jsonBean.result.city);
+                 }
+             });
 
-         System.out.println(response.body().string());
-         JsonBean status = gson.fromJson(response.body().string(), JsonBean.class);
-         Log.i("json------", status.getResult().getData().getPm().getCityName());
+             Log.i("json------", jsonBean.result.city);
+         }catch (Exception e){
+             Log.i("json------", e.getMessage()+"/"+e.getCause());
+         }
+
      }
 
  }
